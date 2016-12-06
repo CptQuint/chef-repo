4060bfb5119c1d5bcf06824bcbf6f5be69311eb0
Andrew Quinton <aquinton@connect-service.co.uk>
2016-12-06 15:56:09 +0000

    Import simple-logstash version 0.2.4

---
+++ cookbooks/simple-logstash/metadata.rb
@@ -0,0 +1,16 @@
+name             'simple-logstash'
+maintainer       'Yauhen Artsiukhou'
+maintainer_email 'jsirex@gmail.com'
+license          'Apache 2'
+description      'Installs/Configures simple-logstash. No less. No more.'
+long_description 'Installs/Configures simple-logstash. No less. No more.'
+issues_url       'https://github.com/jsirex/simple-logstash-cookbook/issues'
+source_url       'https://github.com/jsirex/simple-logstash-cookbook'
+version          '0.2.4'
+
+supports 'debian'
+supports 'ubuntu'
+supports 'centos'
+
+depends 'ark'
+depends 'runit', '>= 1.7.2'
