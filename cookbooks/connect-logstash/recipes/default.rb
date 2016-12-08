48cbea143baaf7d2ba7b96e8b2a5885162f6e809
Andrew Quinton <aquinton@connect-service.co.uk>
2016-12-08 08:53:07 +0000

    Remove redundant cookbook

--- cookbooks/connect-logstash/recipes/default.rb
+++ cookbooks/connect-logstash/recipes/default.rb
@@ -2,7 +2,22 @@
 # Cookbook Name:: connect-logstash
 # Recipe:: default
 #
-# Copyright 2016, YOUR_COMPANY_NAME
+# Copyright 2016, Connect Distribution Services Ltd.
 #
 # All rights reserved - Do Not Redistribute
 #
+
+cookbook_file "/etc/yum.repos.d/logstash.repo" do
+  source "logstash.repo"
+  mode "0644"
+end
+
+include_recipe "yum"
+
+package 'logstash' do
+  action :install
+end
+
+service 'logstash' do
+  action [ :enable, :start ]
+end
--- cookbooks/simple-logstash/libraries/provider_logstash_service.rb
+++
@@ -1,6 +0,0 @@
-class Chef
-  class Provider
-    class LogstashService < RunitService
-    end
-  end
-end
