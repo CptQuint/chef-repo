2ad0c544836cdb84d2baeb5171d0eb354fbd86a8
Andrew Quinton <aquinton@connect-service.co.uk>
2016-12-08 12:03:35 +0000

    Adding supporting files and templates

--- cookbooks/connect-logstash/recipes/default.rb
+++ cookbooks/connect-logstash/recipes/default.rb
@@ -7,12 +7,15 @@
 # All rights reserved - Do Not Redistribute
 #
 
-cookbook_file "/etc/yum.repos.d/logstash.repo" do
-  source "logstash.repo"
-  mode "0644"
-end
-
 include_recipe "yum"
+
+# add the Logstash repository
+yum_repository 'logstash' do
+  description "Logstash repository for 2.2 packages"
+  baseurl "http://packages.elasticsearch.org/logstash/2.2/centos"
+  gpgkey 'http://packages.elasticsearch.org/GPG-KEY-elasticsearch'
+  action :create
+end
 
 package 'logstash' do
   action :install
@@ -20,4 +23,28 @@ end
 
 service 'logstash' do
   action [ :enable, :start ]
+end
+
+execute 'cert_gen' do
+  cwd  '/etc/pki/tls'
+  command "/usr/bin/openssl req -x509 -days 3650 -batch -nodes -newkey rsa:2048 -keyout private/fqdn #{node[:fqdn]}.key -out certs/fqdn #{node[:fqdn]}.crt"
+end
+
+template "02-beats-input.conf" do
+  path "/etc/logstash/conf.d/02-beats-input.conf"
+  source "02-beats-input.conf.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+end
+
+cookbook_file "/etc/logstash/conf.d/10-syslog-filter.conf" do
+  source "10-syslog-filter.conf"
+  mode "0644"
+end
+
+cookbook_file "/etc/logstash/conf.d/30-elasticsearch-output.conf" do
+  source "30-elasticsearch-output.conf"
+  mode "0644"
+  notifies :restart, resources(:service => "logstash")
 end
