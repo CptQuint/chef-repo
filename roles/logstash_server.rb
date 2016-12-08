909eff86351594083084490c7c983171eae861bb
Andrew Quinton <aquinton@connect-service.co.uk>
2016-12-08 16:26:41 +0000

    Syntax woes

--- roles/logstash_server.rb
+++ roles/logstash_server.rb
@@ -1,4 +1,4 @@
 name "logstash_server"
 description "A role to configure Connect House Logstash servers"
 run_list "recipe[yum]", "recipe[java::oracle]", "recipe[connect-logstash]"
-default_attributes( :java => { :oracle => { "accept_oracle_download_terms" => true } } { :jdk_version => { "7" } } )
+default_attributes( :java => { :oracle => { "accept_oracle_download_terms" => true } :jdk_version => { "7" } } )
