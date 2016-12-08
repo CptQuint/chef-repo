8fd39634a84e4de396e24809b0cb7efe002fd143
Andrew Quinton <aquinton@connect-service.co.uk>
2016-12-08 16:24:44 +0000

    Doh

--- roles/logstash_server.rb
+++ roles/logstash_server.rb
@@ -1,5 +1,4 @@
 name "logstash_server"
 description "A role to configure Connect House Logstash servers"
 run_list "recipe[yum]", "recipe[java::oracle]", "recipe[connect-logstash]"
-default_attributes( "java" => { "install_flavor" => "oracle", "jdk_version" => "8", "oracle" => { "accept_oracle_download_terms" => true } } )
 default_attributes( :java => { :oracle => { "accept_oracle_download_terms" => true } } { :jdk_version => { "7" } } )
