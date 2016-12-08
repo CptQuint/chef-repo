480477a692b414ed605f6925a7b81d32c8296655
Andrew Quinton <aquinton@connect-service.co.uk>
2016-12-08 14:07:03 +0000

    Updated metadata

--- cookbooks/connect-logstash/metadata.rb
+++ cookbooks/connect-logstash/metadata.rb
@@ -1,7 +1,9 @@
 name             'connect-logstash'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Connect Distribution Services Ltd'
+maintainer_email 'infrastructurealerts@connect-service.co.uk'
 license          'All rights reserved'
 description      'Installs/Configures connect-logstash'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends "yum"
